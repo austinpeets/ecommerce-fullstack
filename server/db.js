6d90bc54b546cdae7b381d13fcb5ac7ccf078508
Austin Pitts <austinpitts@Austins-Laptop.local>
2024-06-12 23:44:55 -0400

    accessed prodcuts through api endpoint using express router

--- server/db.js
+++ server/db.js
@@ -38,3 +38,4 @@ const seedDataBase = async () => {
 
 seedDataBase()
 
+
--- server/routes/product.js
+++ server/routes/product.js
@@ -1,12 +1,28 @@
+const pg = require('pg')
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/ecommerce_backend');
 const express = require('express')
+const router = express.Router()
 const app = express()
+
 
 app.use( express.json() )
 
-app.get('/products', (req, res) => {
-    console.log('')
-})
+router.get('/', async (req, res) => {
+    try {
+        await client.connect()
+        const result = await client.query('SELECT * FROM products');
+        const products = result.rows;
+        res.json(products);
+      } catch (err) {
+        console.error('Error fetching products:', err);
+        res.status(500).json({ message: 'Internal Server Error' });
+      }
+    });
+
+   
 
 app.get('/products/:id', (req, res) => {
     console.log('')
 })
+
+module.exports = router
