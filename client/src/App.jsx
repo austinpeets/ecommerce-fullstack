129b9ff84f5fb9c573b560ebf3536436f54c5bde
berzyvonne <berzyvonne@gmail.com>
2024-06-28 18:15:35 -0400

    register page mui, register user work in progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
 import Login from './components/Login'
 import Navigations from './components/Navigations'
 import Products from './components/Products'
 import { Routes, Route } from "react-router-dom"
+import { useState } from 'react'
 // import { Link } from "react-router-dom"
 import './App.css'
+import Register from './components/Register'
+import { setToken } from './components/Register'
 
 function App() {
   return (
@@ -19,7 +21,8 @@ function App() {
             {/* <Route path="/home" element={<Home />}></Route> */}
             {/* <Route path="/product/:id"element={<Product />}></Route>  */}
             <Route path="/products" element={<Products />}></Route>
-            <Route path="/login" element={<Login />}></Route>
+            <Route path="/login" element={<Login setToken={setToken} />}></Route>
+            <Route path="/register" element={<Register setToken={setToken}/>}></Route>
           </Routes>/
         </div>
       </div>
