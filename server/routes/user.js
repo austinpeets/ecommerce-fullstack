b3df7e8966340d9b673fb5a4a2fb01bbb9b92f52
Austin Pitts <apitts@ymail.com>
2024-06-28 15:28:53 -0400

    api endpoint: successfully created a working register api endpoint

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,75 +1,67 @@
-const express = require('express')
-const pool = require('../db')
-const app = express()
-const router = express.Router()
-const bcrypt = require('bcrypt')
-const { authenticate } = require('../seed')
-app.use( express.json() )
+const express = require("express");
+const pool = require("../db");
+const app = express();
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const { authenticate } = require("../seed");
+const { createUserAndGenerateToken } = require("../seed");
+app.use(express.json());
 
-
-router.get('/', async (req, res) => {
-    try {
-        const result = await pool.query('SELECT * FROM users');
-        const users = result.rows;
-        res.json(users)
-    } catch(err) {
-        console.error('Error fetching users:', err);
-        res.status(500).json({ message: 'Internal Server Error'})
-    }
-   
-})
-
-router.get('/:id', async (req, res) => {
-    const userID = req.params.id;
-
-    try {
-        const result = await pool.query('SELECT * FROM users WHERE id = $1', [userID]);
-        if (result.rows.length === 0) {
-            res.status(404).json({ message: 'User not found'})
-        } else {
-            res.json(result.rows[0])
-        }
-    } catch(err) {
-        console.error('Error fetching user by id', err);
-        res.status(500).json({ message: 'Internal Server Error'})
-    }
-})
-
-router.post('/login', async (req, res, next) => {
-    try{
-        res.send(await authenticate(req.body));
-    }
-    catch(err) {
-        next(err)
-    }
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM users");
+    const users = result.rows;
+    res.json(users);
+  } catch (err) {
+    console.error("Error fetching users:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
-    
 
-router.post('/register', async (req, res) => {
-    const {name, email, password} = req.body
+router.get("/:id", async (req, res) => {
+  const userID = req.params.id;
 
-    if(!name || !email || !password) {
-        return res.status(400).json({ message: "Please provide all required fields"})
+  try {
+    const result = await pool.query("SELECT * FROM users WHERE id = $1", [
+      userID,
+    ]);
+    if (result.rows.length === 0) {
+      res.status(404).json({ message: "User not found" });
+    } else {
+      res.json(result.rows[0]);
     }
+  } catch (err) {
+    console.error("Error fetching user by id", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
-    try {
-        const hashedPassword = await bcrypt.hash(password, 12)
+router.post("/login", async (req, res, next) => {
+  try {
+    res.send(await authenticate(req.body));
+  } catch (err) {
+    next(err);
+  }
+});
 
-        const query = `
-        INSERT INTO users (name, email, password)
-        VALUES ($1, $2, $3)
-        RETURNING id, name, email
-        `;
-        
-        const values = [name, email, hashedPassword];
-        const result = await pool.query(query, values);
-        const newUser = result.rows[0];
-        res.status(201).json({ message: 'User registered successfully', user: newUser});
-    } catch(err) {
-        console.error('Error registering user', err)
-        res.status(500).json({ message: 'Internal Server Error'})
-    }
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
 
-})
+  if (!name || !email || !password) {
+    return res
+      .status(400)
+      .json({ message: "Please provide all required fields" });
+  }
+
+  try {
+   const{token , user} = await createUserAndGenerateToken(req.body);
+    res
+      .status(201)
+      .json({ message: "User registered successfully", user, token });
+  } catch (err) {
+    console.error("Error registering user", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
